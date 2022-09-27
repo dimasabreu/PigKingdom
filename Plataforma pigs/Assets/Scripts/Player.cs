d10addfd12759e0fc740b419d79e47d57080f24f
Dimas Abreu <dimasabreu@live.com>
2022-09-27 15:12:49 -0300

    Player update

--- Plataforma pigs/Assets/Scripts/Player.cs
+++ Plataforma pigs/Assets/Scripts/Player.cs
@@ -5,12 +5,14 @@ public class Player : MonoBehaviour
     [Header ("Funções primarias")]
     private Rigidbody2D meuRB;
     private SpriteRenderer SpritePlayer;
+    private Animator minhaAnimacao;
     [SerializeField] private float VeloDeMov = 5f;
     // Start is called before the first frame update
     void Start()
     {
         meuRB = GetComponent<Rigidbody2D>();
         SpritePlayer = GetComponent<SpriteRenderer>();
+        minhaAnimacao = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,15 +25,21 @@ public class Player : MonoBehaviour
     {
         var movimento = Input.GetAxis("Horizontal") * VeloDeMov;
         meuRB.velocity = new Vector2(movimento, meuRB.velocity.y);
-        if(Input.GetKeyDown(KeyCode.A))
+        if(Input.GetKey(KeyCode.A))
         {
+            minhaAnimacao.SetBool("Movendo",true);
             // fazendo a sprite virar para a esquerda
             SpritePlayer.flipX = true;
         }
-        if(Input.GetKeyDown(KeyCode.D))
+        else if(Input.GetKey(KeyCode.D))
         {
+            minhaAnimacao.SetBool("Movendo",true);
             //fazendo a sprite virar para a direta
             SpritePlayer.flipX = false;
+        }
+        else
+        {
+            minhaAnimacao.SetBool("Movendo",false);
         }
         
     }
