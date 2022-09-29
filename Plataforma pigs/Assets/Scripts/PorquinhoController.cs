73065b9e72bfb1159e8c72008708b579778c36d1
Dimas Abreu <dimasabreu@live.com>
2022-09-29 11:43:43 -0300

    Update PorquinhoController.cs

--- Plataforma pigs/Assets/Scripts/PorquinhoController.cs
+++ Plataforma pigs/Assets/Scripts/PorquinhoController.cs
@@ -6,36 +6,69 @@ public class PorquinhoController : MonoBehaviour
 {
     // Start is called before the first frame update
     private Rigidbody2D meuRB;
-    [SerializeField] private float speed = 3f;
+    private Animator minhaAnimacao;
+    private BoxCollider2D boxCol;
+    [SerializeField] private LayerMask layerLevel;
+    [SerializeField] private float speedh = 3f;
     [SerializeField] private float espera = 2f;
-    [SerializeField] private GameObject maoDireita;
-    [SerializeField] private GameObject maoEsquerda;
+   
     void Start()
     {
         meuRB = GetComponent<Rigidbody2D>();
-        
+        minhaAnimacao = GetComponent<Animator>();
+        boxCol = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+    private void FixedUpdate() 
     {
         Movendo();
     }
 
     private void Movendo()
     {
+        // mudando de direcao se bater na parede
+        if (Parede())
+        {
+            meuRB.velocity = new Vector2(meuRB.velocity.x * -1f, meuRB.velocity.y);
+        }
+        // olhando para onde está indo
+        if (meuRB.velocity.x != 0)
+            {
+                transform.localScale = new Vector3(Mathf.Sign(meuRB.velocity.x) * -1, 1f, 1f);
+            }
         //movendo se o tempo acabar
         if (espera <= 0f)
         {
-            meuRB.velocity = new Vector2(speed, meuRB.velocity.y);
-            // olhando para onde está indo
-            transform.localScale = new Vector3(Mathf.Sign(meuRB.velocity.x) * -1, 1f, 1f);
-            espera = Random.Range(2f, 10f);
+            //movendo
+            int dir = Random.Range(-1, 2);
+            // multiplicando a velocidade pela dir
+            meuRB.velocity = new Vector2(speedh * dir, meuRB.velocity.y);
+            
+            
+            //resetando a espera
+            espera = Random.Range(2f, 4f);
+            
         }
         // se a espera for maior do q zero eu diminuo ela
         else
         {
-            espera--;
+            espera -= Time.deltaTime;
         }
+
+        // checando se estou me movendo
+        minhaAnimacao.SetBool("Movendo", meuRB.velocity.x != 0);
+    }
+
+    private bool Parede()
+    {
+        var dir = new Vector2(Mathf.Sign(meuRB.velocity.x), 0f);
+        bool parede = Physics2D.Raycast(boxCol.bounds.center, dir, 1f, layerLevel);
+        Debug.DrawRay(boxCol.bounds.center, dir * 1f, Color.red);
+        return parede;
     }
 }
