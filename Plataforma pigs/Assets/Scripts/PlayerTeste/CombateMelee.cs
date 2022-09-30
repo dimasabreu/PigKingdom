1428899e74adb2dd4dbe139c144f0495932b6bd5
Dimas Abreu <dimasabreu@live.com>
2022-09-30 12:12:32 -0300

    Update CombateMelee.cs

--- Plataforma pigs/Assets/Scripts/PlayerTeste/CombateMelee.cs
+++ Plataforma pigs/Assets/Scripts/PlayerTeste/CombateMelee.cs
@@ -4,12 +4,46 @@ using UnityEngine;
 
 public class CombateMelee : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision) 
-    {
-        if(collision.gameObject.CompareTag("Inimigo"))
+   private float timeBtwAttack;
+   public float startTimeBtwAttack;
+
+   public Transform attackPos;
+   public LayerMask whatIsEnemies;
+   public float attackRange;
+   public int dano;
+
+   public Animator anim;
+
+   void Update() 
+   {
+        if(timeBtwAttack <= 0)
         {
-            collision.gameObject.GetComponent<PorquinhoController>().perdeVida(1);
-            collision.GetComponentInParent<Animator>().SetTrigger("DanoMorte");
+            if(Input.GetButtonDown("Fire1"))
+            {
+                anim.SetBool("Batendo", true);
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for(int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponentInParent<PorquinhoController>().perdeVida(dano);
+                }
+                timeBtwAttack = startTimeBtwAttack;
+            }
+            
+            else
+            {
+                anim.SetBool("Batendo", false);
+            }
         }
-    }
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+            
+        }
+   }
+
+   private void OnDrawGizmosSelected() 
+   {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+   }
 }
